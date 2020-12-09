aa1578f64e5d09d20fc9275edb38b49e316202d2
liyantao <li.yantao@shuzhi.ai>
2020-12-09 18:23:18 +0800

    修改首页

--- src/views/bank/data-panel/index.vue
+++ src/views/bank/data-panel/index.vue
@@ -1,3 +1,19 @@
 <template>
   <div>首页11111</div>
 </template>
+<script>
+export default {
+  name:'name',
+  data(){
+    return {
+      message: 'hello'
+    }
+  },
+  methods:{
+    fun() {
+      let a = '2222'
+      console.log(a)
+    }
+  }
+}
+</script>
